4d5a7ffcbc67891daf6cdbe3dcb4782bafdb8574
Busna27 <lucabusnelli2001@gmail.com>
2022-03-01 16:40:38 +0100

    looped carousel

--- js/script.js
+++ js/script.js
@@ -17,15 +17,24 @@ prevBtn.addEventListener('click', function() {
 
     activeElement--;
 
+    if(activeElement < 0) {
+        activeElement = 4;
+    }
+
     carouselElements[activeElement].classList.add('my-active');
     bookmarkElements[activeElement].classList.add('my-active-circle');
 });
+
 
 nextBtn.addEventListener('click', function() {
     carouselElements[activeElement].classList.remove('my-active');
     bookmarkElements[activeElement].classList.remove('my-active-circle');
 
     activeElement++;
+
+    if(activeElement > 4) {
+        activeElement = 0;
+    }
 
     carouselElements[activeElement].classList.add('my-active');
     bookmarkElements[activeElement].classList.add('my-active-circle');
