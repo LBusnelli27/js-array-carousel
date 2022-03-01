b690ae20f09428cb1f03161cf8f599532582b609
Busna27 <lucabusnelli2001@gmail.com>
2022-03-01 16:55:20 +0100

    add miniature under the caruosel img

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const carouselElements = document.getElementsByClassName('my-carousel-item');
 const bookmarkElements = document.getElementsByClassName('my-circle');
+const miniatureElements = document.getElementsByClassName('my-miniature-item');
 
 carouselElements[0].classList.add('my-active');
 bookmarkElements[0].classList.add('my-active-circle');
-console.log(carouselElements);
+miniatureElements[0].classList.add('my-opacity-min')
 
 
 const prevBtn = document.getElementById('btn-prev');
@@ -14,6 +15,7 @@ let activeElement = 0;
 prevBtn.addEventListener('click', function() {
     carouselElements[activeElement].classList.remove('my-active');
     bookmarkElements[activeElement].classList.remove('my-active-circle');
+    miniatureElements[activeElement].classList.remove('my-opacity-min');
 
     activeElement--;
 
@@ -23,12 +25,14 @@ prevBtn.addEventListener('click', function() {
 
     carouselElements[activeElement].classList.add('my-active');
     bookmarkElements[activeElement].classList.add('my-active-circle');
+    miniatureElements[activeElement].classList.add('my-opacity-min');
 });
 
 
 nextBtn.addEventListener('click', function() {
     carouselElements[activeElement].classList.remove('my-active');
     bookmarkElements[activeElement].classList.remove('my-active-circle');
+    miniatureElements[activeElement].classList.remove('my-opacity-min');
 
     activeElement++;
 
@@ -38,4 +42,5 @@ nextBtn.addEventListener('click', function() {
 
     carouselElements[activeElement].classList.add('my-active');
     bookmarkElements[activeElement].classList.add('my-active-circle');
+    miniatureElements[activeElement].classList.add('my-opacity-min');
 });
